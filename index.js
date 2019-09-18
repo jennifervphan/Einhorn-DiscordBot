7c74b2bfe4584c49324589fe5d8796b1afff7afc
Niklas Eicker <nikle1@gmx.de>
2019-09-18 19:30:42 +0200

    rough implementation for commands

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const Discord = require('discord.js');
+const CommandSystem = require('./commandSystem')
 
 require('dotenv').config();
 const client = new Discord.Client();
+console.log(CommandSystem)
+const commandSystem = new CommandSystem()
+
+client.on('message', event => {
+    console.log(event.content)
+    commandSystem.handleEvent(event);
+})
 
 require('./events/ready')(client)
 require('./events/awesome')(client)
